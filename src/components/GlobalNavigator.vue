8ebd4bd8e1f1daccb3d4279caa9922829440e84d
paupim <ppimasga36@alumnes.ub.edu>
2023-05-23 20:07:25 +0200

    perfil y test

--- src/components/GlobalNavigator.vue
+++ src/components/GlobalNavigator.vue
@@ -3,7 +3,7 @@
         <p class="col-sm-8"></p>
         <a href="#" class="col-sm-1 p-1 py-2 text-dark">Fitness</a>
         <a href="#" class="col-sm-1 p-1 py-2 text-dark">Alimentos</a>
-        <a href="#" class="col-sm-1 p-1 py-2 text-dark">Perfil</a>
+        <a href="profile.vue" class="col-sm-1 p-1 py-2 text-dark">Perfil</a>
 
     </nav>
 </template>
