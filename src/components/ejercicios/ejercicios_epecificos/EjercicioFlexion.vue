3acc19d89247b5807daf35724a5a4fb42abf747e
Arnau Belmonte Juliachs <arnau@debian.local.com>
2023-06-17 18:50:42 +0200

    Fix

--- src/components/ejercicios/ejercicios_epecificos/EjercicioFlexion.vue
+++ src/components/ejercicios/ejercicios_epecificos/EjercicioFlexion.vue
@@ -1,25 +1,6 @@
 <template>
   <div class="content">
-    <h1>Flexiones</h1>
-    <ol>
-      <li><strong>Colocamos las manos debajo de los hombros</strong>: los brazos extendidos y con los dedos hacia delante.
-        Las piernas también están estiradas, con los pies ligeramente juntos y apoyados sobre las puntas de los dedos.
-      </li>
-      <li><strong>Mantenemos la espalda recta</strong>: hay que evitar arquear la espalda hacia arriba o que esta se hunda
-        en una posición que suele adoptarse de forma inconsciente cuando cuesta mantener la postura correcta durante un
-        tiempo. Puede ayudar mantener las abdominales apretadas.</li>
-      <li><strong>Colocamos la cabeza mirando hacia abajo evitando hacer fuerza con las cervicales:</strong> el cuerpo y
-        la cabeza deben formar una tabla recta, cabeza y columna vertebral deben quedar alineadas.</li>
-
-      <p>La flexión consiste en:</p>
-      <li><strong>Mirar hacia abajo</strong> y bajar lentamente el cuerpo hasta que los codos lleguen a los 90 grados y
-        hasta que el pecho toque el suelo.</li>
-      <li><strong>Presionar hacia arriba con los brazos</strong> hasta que estos estén completamente estirados y se vuelva
-        a la postura inicial.</li>
-      <li><strong>Repetir el movimiento hacia abajo</strong>. Es posible empezar haciendo pocas flexiones (dos, cinco,
-        diez…), las que puedas hacer de forma adecuada, e ir aumentando el número a medida que se desarrolla más fuerza y
-        técnica.</li>
-    </ol>
+    
   </div>
 </template>
 
